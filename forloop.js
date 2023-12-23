57581846165025ca75c2d86c9ce526a9b22e4a43
Muhammad Sohail <145179902+MuhammadSohail1122@users.noreply.github.com>
2023-12-23 20:23:11 +0500

    using for loop for invitation more than one person

---
+++ forloop.js
@@ -0,0 +1,4 @@
+let friends = ['sohail','khan','Ahmad','Danial','azhar','Aftab','uzair']
+for(let a=0;a<10;a++){
+    console.log(`Hello dear Friend Mr.${friends[a]} you are invited at a party today`)
+}
